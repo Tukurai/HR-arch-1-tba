8c4175fe99460a9cdbf8a27a032b257355f04f3d
Extrasolar99 <itsyaboyextrasolar@gmail.com>
2023-09-25 15:43:08 +0200

    Class fix

--- TBAServer/Database/Models/floor.py
+++ TBAServer/Database/Models/floor.py
@@ -1,20 +1,21 @@
-
-class Floor:  
-    def __init__(self, _id, name, rooms=None):
-        self._id = _id
+class Floor:
+    def __init__(self, id, name, rooms=None):
+        self._id = id
         self.name = name
-        self.rooms = rooms if rooms is not None else []  
+        self.rooms = rooms if rooms is not None else []
 
-    def add_room(self, room):  
-        self.rooms.append(room)  
+    def add_room(self, room):
+        if self._id is not None:
+            self.rooms.append(room)
 
-    # This method converts the Floor instance to a dictionary.  
-    def to_dict(self):  
-        # Store only the _id of each Room instance in rooms.  
-        room_ids = [room._id for room in self.rooms]  
-        return {  
-            "_id": self._id,  
-            "name": self.name,  
-            "rooms": room_ids  
-        }  
-    
+    # This method converts the Floor instance to a dictionary.
+    def to_dict(self):
+        # Store only the _id of each Room instance in rooms.
+        room_ids = [room._id for room in self.rooms]
+
+        data = {"name": self.name, "rooms": room_ids}
+
+        if self._id is not None:
+            data["_id"] = self._id
+
+        return data
