05418eaa1239668c502a3e45c892e7f6bc4315b3
Jeroen Faas <info@toxi.us>
2023-09-25 14:23:08 +0200

    Idiot fixed bugs

--- TBAServer/Database/Models/player.py
+++ TBAServer/Database/Models/player.py
@@ -1,17 +1,18 @@
-class Player:  
-    def __init__(self, _id, name, password):
-        self._id = _id
+class Player:
+    def __init__(self, id, name, password):
+        self._id = id
         self.name = name
         self.password = password
-        
-    def to_dict(self):  
-        return {  
-            "_id": self._id,  
-            "name": self.name,  
-            "password": self.password  
-        }  
-    
-    # This static method creates a new Player instance from a dictionary.  
-    @staticmethod  
-    def from_dict(data):  
-        return Player(data["_id"], data["name"], data["password"]) 
+
+    def to_dict(self):
+        data = {"name": self.name, "password": self.password}
+
+        if self._id is not None:
+            data["_id"] = self._id
+
+        return data
+
+    # This static method creates a new Player instance from a dictionary.
+    @staticmethod
+    def from_dict(data):
+        return Player(data["_id"], data["name"], data["password"])
